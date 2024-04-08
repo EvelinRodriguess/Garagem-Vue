bcb1fab3836efd643b1cbb9b6db783104ec90350
EvelinRodriguess <evelinrodrigues.ifc@gmail.com>
2024-04-08 12:01:46 -0300

    cor e acessorio

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import router from './router';
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
     <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/acessorios">Acessorio</router-link> 
+    <router-link to="/cores">Cor</router-link> 
   </header>
   <main>
     <RouterView />
